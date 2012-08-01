97dd2d31b0e967c089dfd6159aa47fc86bbdcdb1
Adam Greenhall <adam.greenhall@gmail.com>
2012-07-31 20:52:30 -0700

    fix links to piece in issue (issue#piece)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def get_issue_from_title(title)
-    title=title.gsub('-',' ').gsub('_',' ')
+    title=title.split('#').first.gsub('-',' ').gsub('_',' ')
     Issue.limit(1).where("title like ?", "%#{title}%").first
   end
   def get_author_from_name(name)
