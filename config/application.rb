2ddf05923f6a4c80d89822efc80ae1aa0dd1498d
Adam Greenhall <adam.greenhall@gmail.com>
2012-08-07 21:30:40 -0700

    heroku config settings

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,7 @@ module Orq
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false # for heroku
   end
 end
