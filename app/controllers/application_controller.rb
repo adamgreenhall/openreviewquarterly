2bafee217568ca4dd484516a1dc5eebd95ba5dad
Adam Greenhall <adam.greenhall@gmail.com>
2012-08-09 08:18:40 -0700

    make name matching case insensitive (by converting to uppercase)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   def get_issue_from_title(title)
     title=title.split('#').first.gsub('-',' ').gsub('_',' ')
-    Issue.limit(1).where("title like ?", "%#{title}%").first
+    Issue.limit(1).where("upper(title) like ?", "%#{title.upcase}%").first
   end
   def get_author_from_name(name)
     name=name.gsub('-',' ').gsub('_',' ')
     first,last=name.split
-    author=Author.limit(1).where("first_name like ? AND last_name like ?", "%#{first}%","%#{last}").first
+    author=Author.limit(1).where("upper(first_name) like ? AND upper(last_name) like ?", "%#{first.upcase}%","%#{last.upcase}").first
     if author.nil? 
       # try a looser search condition 
-      return Author.limit(1).where("first_name like ? OR last_name like ?", "%#{first}%","%#{last}").first
+      return Author.limit(1).where("upper(first_name) like ? OR upper(last_name) like ?", "%#{first.upcase}%","%#{last.upcase}").first
     else
       return author
     end
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   def get_piece_from_titles(params)
      issue=get_issue_from_title(params[:issue_title])
      title=params[:piece_title].gsub('-',' ').gsub('_',' ')
-     issue.pieces.where("title like ?","%#{title}").limit(1).first
+     issue.pieces.where("upper(title) like ?","%#{title.upcase}").limit(1).first
   end
 
 end
