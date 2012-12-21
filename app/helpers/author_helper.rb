8a88037ccc05d9bee030a0b77ee9673067d99c6c
Adam Greenhall <adam.greenhall@gmail.com>
2012-12-21 14:48:25 -0800

    only use first name to get author image, no middle name

--- app/helpers/author_helper.rb
+++ app/helpers/author_helper.rb
@@ -1,7 +1,7 @@
 module AuthorHelper
   def author_photo(author)
     begin 
-      image_tag 'authors/'+urlify(author.name,'_')+'.jpg'
+      image_tag 'authors/'+urlify("{author.first_name.split.first} {author.last_name}",'_')+'.jpg'
     rescue
       image_tag 'authors/missing_author_photo.jpg'
     end
