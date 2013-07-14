de3b0acea0289325e8967770779a0cdb3450ccbd
Adam Greenhall <adam.greenhall@gmail.com>
2013-07-14 15:08:05 -0700

    resize author images

--- app/assets/images/authors/resample.py
+++ app/assets/images/authors/resample.py
@@ -4,3 +4,9 @@ from os import system
 for fnm in glob('new/*.png'):
     newfnm = fnm.split('/')[1].replace('.png', '.jpg')
     system('convert {} {}'.format(fnm, newfnm))
+
+for fnm in glob('*.jpg'):
+    # resize pictures to a resonable max area
+    # imagemagick.org/Usage/resize/#pixel
+    max_size = 240
+    system('convert {f} -resize {ms}@ {f}'.format(f=fnm, ms=max_size**2))
